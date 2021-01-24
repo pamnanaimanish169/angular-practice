407d35e3b4dd6853aa6fb9e995668034f86e26af
manish <manishpamnani169@gmail.com>
2021-01-24 12:33:46 +0530

    Changed folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './modules/forms/forms.component';
+import { ApiComponent } from './modules/api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule
